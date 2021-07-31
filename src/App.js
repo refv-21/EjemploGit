0bf5f1c0b4064187b7e53a8aa2dcb5d9ab91f2bb
refv-21 <killahvue@gmail.com>
2021-07-31 17:08:34 -0600

    Guía 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aquí haremos nuestro To-Do List
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form />
       </header>
     </div>
   );
